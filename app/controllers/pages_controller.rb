c1d037560c8df34a23573593a430a319f6bce471
lesage jeremy <lesage1510@gmail.com>
2021-06-16 16:56:08 +0200

    Home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,17 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def qui_sommes_nous
+  end
+
+  def acheter_une_voiture
+  end
+
+  def louer_une_voiture
+  end
+
+  def vendre_une_voiture
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get '/qui_sommes_nous', to: 'pages#qui_sommes_nous'
+  get '/acheter_une_voiture', to: 'pages#acheter_une_voiture'
+  get '/louer_une_voiture', to: 'pages#louer_une_voiture'
+  get '/vendre_une_voiture', to: 'pages#vendre_une_voiture'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
